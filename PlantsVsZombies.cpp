8e59efb61da011abc86d05a26dae3616f09e7964
Abkreno <mohamedelhamamsy789@gmail.com>
2015-11-05 23:39:30 +0200

    Adds Struct for Tiles

--- PlantsVsZombies.cpp
+++ PlantsVsZombies.cpp
@@ -1,101 +1,46 @@
 #include <glut.h>
 #include <stdio.h>
-float rotAng,angX,angY,angZ,angX2,angY2,angZ2;
-void drawGrid() {
+#define WINDOW_WIDTH  600
+#define WINDOW_HEIGHT 600
 
-}
+struct Tile {
+	float x, y, z, r, b, g;
+	bool highlighted, occupied;
+	Tile(float x, float y, float z, float r, float b, float g) {
+		this->x = x;
+		this->y = y;
+		this->z = z;
+		this->r = r;
+		this->b = b;
+		this->g = g;
+		highlighted = false;
+		occupied = false;
+	}
+};
 void Display(void) {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-	//drawGrid();
-
 	glPushMatrix();
-
-	glColor3f(0.2, 0.1, 0.1);
-	glPushMatrix();
-	glScalef(1, 0.05, 1);
-	glTranslatef(-2.0, 0, 2.0);
-	glutSolidCube(2);
+	glColor3f(0.0f, 0.0f, 0.0f);
+	glutSolidCube(1);
 	glPopMatrix();
-
-	glPushMatrix();
-	glScalef(1, 0.05, 1);
-	glTranslatef(0.0, 0, 2.0);
-	glutSolidCube(2);
-	glPopMatrix();
-	glColor3f(0.5f, 0.5f, 0.5f);
-	glScalef(1, 0.01, 1);
-	glTranslatef(0, 0, 0);
-	glutSolidCube(6);
-
-	glPopMatrix();
-
-	glPushMatrix();
-	glRotatef(-rotAng, 0, 1, 0);
-	glTranslatef(2, 0, 0);
-	glRotatef(rotAng, 1, 0, 0);
-	glColor3f(0.5f, 0.5f, 0.5f);
-	//glutSolidSphere(0.5, 25, 25);
-	glPopMatrix();
-
 	glFlush();
 }
 
 void Anim() {
-	rotAng += 0.01;
 	glLoadIdentity();
-	gluLookAt(angX, angY, angZ, angX2, angY2, angZ2, 0.0f, 1.0f, 0.0f);
+	
 	glutPostRedisplay();
 }
 void key(unsigned char key, int x, int y) {
 	
-	if (key == 49) {
-		angX += 0.1;
-	}
-	if (key == 50) {
-		angY += 0.1;
-	}
-	if (key == 51) {
-		angZ += 0.1;
-	}
-	if (key == 52) {
-		angX2 += 0.1;
-	}
-	if (key == 53) {
-		angY2 += 0.1;
-	}
-	if (key == 54) {
-		angZ2 += 0.1;
-	}
-	
-	if (key == 113) {
-		angX -= 0.1;
-	}
-	if (key == 119) {
-		angY -= 0.1;
-	}
-	if (key == 101) {
-		angZ -= 0.1;
-	}
-	if (key == 114) {
-		angX2 -= 0.1;
-	}
-	if (key == 116) {
-		angY2 -= 0.1;
-	}
-	if (key == 121) {
-		angZ2-= 0.1;
-	}
-	printf("x = %.2f y = %.2f z = %.2f\n", angX,angY,angZ);
-	printf("x = %.2f y = %.2f z = %.2f\n\n", angX2, angY2, angZ2);
 }
 void main(int argc, char** argv) {
 	glutInit(&argc, argv);
 
-	glutInitWindowSize(500, 500);
-	glutInitWindowPosition(150, 150);
+	glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+	glutInitWindowPosition(350, 350);
 
-	glutCreateWindow("OpenGL - 3D Template");
+	glutCreateWindow("Plants vs Zombies");
 	glutDisplayFunc(Display);
 	glutIdleFunc(Anim);
 	glutKeyboardFunc(key);
@@ -106,15 +51,9 @@ void main(int argc, char** argv) {
 
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	gluPerspective(45.0f, 500 / 500, 0.01f, 500.0f);
+	gluPerspective(45.0f, WINDOW_WIDTH / WINDOW_HEIGHT, 0.01f, 600.0f);
 
 	glMatrixMode(GL_MODELVIEW);
-	
-	angX = 7;
-	angY = 7;
-	angZ = 0;
-	angX2 = 0;
-	angY2 = 0;
-	angZ2 = 0.01f;
+
 	glutMainLoop();
 }
