38d6864bb6cc34af5db7dced201c4a81a0df53d6
Abkreno <mohamedelhamamsy789@gmail.com>
2015-11-05 23:49:59 +0200

    Adds draw function for tiles

--- PlantsVsZombies.cpp
+++ PlantsVsZombies.cpp
@@ -3,32 +3,40 @@
 #define WINDOW_WIDTH  600
 #define WINDOW_HEIGHT 600
 
+const int gridRows = 5, gridCols = 7;
+
 struct Tile {
 	float x, y, z, r, b, g;
 	bool highlighted, occupied;
-	Tile(float x, float y, float z, float r, float b, float g) {
+	Tile(float x, float y, float z, float r, float g, float b) {
 		this->x = x;
 		this->y = y;
 		this->z = z;
 		this->r = r;
-		this->b = b;
 		this->g = g;
+		this->b = b;
 		highlighted = false;
 		occupied = false;
+	}
+
+	void draw() {
+		glPushMatrix();
+		glTranslatef(x, y, z);
+		glColor3f(r, g, b);
+		glScalef(1.0f, 0.05f, 1.0f);
+		glutSolidCube(1);
+		glPopMatrix();
 	}
 };
 void Display(void) {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glPushMatrix();
-	glColor3f(0.0f, 0.0f, 0.0f);
-	glutSolidCube(1);
-	glPopMatrix();
+	
 	glFlush();
 }
 
 void Anim() {
 	glLoadIdentity();
-	
+	gluLookAt(7.0f, 7.0f, 0.0f, 0.0f, 0.0f, 0.01f, 0.0f, 1.0f, 0.0f);
 	glutPostRedisplay();
 }
 void key(unsigned char key, int x, int y) {
@@ -38,7 +46,7 @@ void main(int argc, char** argv) {
 	glutInit(&argc, argv);
 
 	glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
-	glutInitWindowPosition(350, 350);
+	glutInitWindowPosition(350, 100);
 
 	glutCreateWindow("Plants vs Zombies");
 	glutDisplayFunc(Display);
