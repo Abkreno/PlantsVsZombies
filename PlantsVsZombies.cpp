17197142542a456ab9a08e6a78a3b954ab04bb6e
Abkreno <mohamedelhamamsy789@gmail.com>
2015-11-10 23:48:41 +0200

    Restores Replaced Code

--- PlantsVsZombies.cpp
+++ PlantsVsZombies.cpp
@@ -1,289 +1,190 @@
-#include <math.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <glut.h>
+#include <stdio.h>
+#include <graphics.h>
 
-#define GLUT_KEY_ESCAPE 27
-#define DEG2RAD(a) (a * 0.0174532925)
+#define WINDOW_WIDTH  600
+#define WINDOW_HEIGHT 600
 
-class Vector3f {
-public:
-	float x, y, z;
+float zoom = 16.0f;
+float rotx = 45;
+float roty = 90.001f;
+float rotz = 0;
+int lastx = 0;
+int lasty = 0;
+unsigned char Buttons[3] = { 0 };
+const int gridRows = 5, gridCols = 9;
+bool view = false;
+Tile tiles[gridRows][gridCols];
 
-	Vector3f(float _x = 0.0f, float _y = 0.0f, float _z = 0.0f) {
-		x = _x;
-		y = _y;
-		z = _z;
-	}
-
-	Vector3f operator+(Vector3f &v) {
-		return Vector3f(x + v.x, y + v.y, z + v.z);
-	}
-
-	Vector3f operator-(Vector3f &v) {
-		return Vector3f(x - v.x, y - v.y, z - v.z);
-	}
-
-	Vector3f operator*(float n) {
-		return Vector3f(x * n, y * n, z * n);
-	}
-
-	Vector3f operator/(float n) {
-		return Vector3f(x / n, y / n, z / n);
-	}
-
-	Vector3f unit() {
-		return *this / sqrt(x * x + y * y + z * z);
-	}
-
-	Vector3f cross(Vector3f v) {
-		return Vector3f(y * v.z - z * v.y, z * v.x - x * v.z, x * v.y - y * v.x);
-	}
-};
-
-class Camera {
-public:
-	Vector3f eye, center, up;
-
-	Camera(float eyeX = 1.0f, float eyeY = 1.0f, float eyeZ = 1.0f, float centerX = 0.0f, float centerY = 0.0f, float centerZ = 0.0f, float upX = 0.0f, float upY = 1.0f, float upZ = 0.0f) {
-		eye = Vector3f(eyeX, eyeY, eyeZ);
-		center = Vector3f(centerX, centerY, centerZ);
-		up = Vector3f(upX, upY, upZ);
-	}
-
-	void moveX(float d) {
-		Vector3f right = up.cross(center - eye).unit();
-		eye = eye + right * d;
-		center = center + right * d;
-	}
-
-	void moveY(float d) {
-		eye = eye + up.unit() * d;
-		center = center + up.unit() * d;
-	}
-
-	void moveZ(float d) {
-		Vector3f view = (center - eye).unit();
-		eye = eye + view * d;
-		center = center + view * d;
-	}
-
-	void rotateX(float a) {
-		Vector3f view = (center - eye).unit();
-		Vector3f right = up.cross(view).unit();
-		view = view * cos(DEG2RAD(a)) + up * sin(DEG2RAD(a));
-		up = view.cross(right);
-		center = eye + view;
-	}
-
-	void rotateY(float a) {
-		Vector3f view = (center - eye).unit();
-		Vector3f right = up.cross(view).unit();
-		view = view * cos(DEG2RAD(a)) + right * sin(DEG2RAD(a));
-		right = view.cross(up);
-		center = eye + view;
-	}
-
-	void look() {
-		gluLookAt(
-			eye.x, eye.y, eye.z,
-			center.x, center.y, center.z,
-			up.x, up.y, up.z
-		);
-	}
-};
-
-Camera camera;
-
-void drawWall(double thickness) {
+void drawHouse() {
+	//Roof
 	glPushMatrix();
-	glTranslated(0.5, 0.5 * thickness, 0.5);
-	glScaled(1.0, thickness, 1.0);
-	glutSolidCube(1);
+	glColor3f(0.1f, 0.01f, 0.5f);
+	glTranslatef(0.0f, 2.0f, -5.5f);
+	glScalef(4.0f, 1.0f, 2.0f);
+	glRotatef(45.0f, 0.0f, 1.0f, 0.0f);
+	glutSolidOctahedron();
+	glPopMatrix();
+
+	//Base
+	glPushMatrix();
+	glColor3f(0.1f, 0.2f, 0.2f);
+	glScalef(2.0f, 1.0f, 1.0f);
+	glTranslatef(0.0f, 1.0f, -5.5f);
+	glutSolidCube(2);
 	glPopMatrix();
 }
-void drawTableLeg(double thick, double len) {
+void drawGrid() {
+	
 	glPushMatrix();
-	glTranslated(0, len / 2, 0);
-	glScaled(thick, len, thick);
-	glutSolidCube(1.0);
-	glPopMatrix();
-}
-void drawJackPart() {
-	glPushMatrix();
-	glScaled(0.2, 0.2, 1.0);
-	glutSolidSphere(1, 15, 15);
-	glPopMatrix();
-	glPushMatrix();
-	glTranslated(0, 0, 1.2);
-	glutSolidSphere(0.2, 15, 15);
-	glTranslated(0, 0, -2.4);
-	glutSolidSphere(0.2, 15, 15);
-	glPopMatrix();
-}
-void drawJack() {
-	glPushMatrix();
-	drawJackPart();
-	glRotated(90.0, 0, 1, 0);
-	drawJackPart();
-	glRotated(90.0, 1, 0, 0);
-	drawJackPart();
-	glPopMatrix();
-}
-void drawTable(double topWid, double topThick, double legThick, double legLen){
-	glPushMatrix();
-	glTranslated(0, legLen, 0);
-	glScaled(topWid, topThick, topWid);
-	glutSolidCube(1.0);
-	glPopMatrix();
-
-	double dist = 0.95*topWid / 2.0 - legThick / 2.0;
-	glPushMatrix();
-	glTranslated(dist, 0, dist);
-	drawTableLeg(legThick, legLen);
-	glTranslated(0, 0, -2 * dist);
-	drawTableLeg(legThick, legLen);
-	glTranslated(-2 * dist, 0, 2 * dist);
-	drawTableLeg(legThick, legLen);
-	glTranslated(0, 0, -2 * dist);
-	drawTableLeg(legThick, legLen);
+	
+	for (int i = 0; i < gridRows; i++) {
+		for (int j = 0; j < gridCols; j++) {
+			tiles[i][j].draw();
+		}
+	}
+	
+	glColor3f(0.3, 0.3, 0.6);
+	glScalef(gridRows, 0.01f, gridCols);
+	glutSolidCube(2);
 	glPopMatrix();
 }
 
-void setupLights() {
-	GLfloat ambient[] = { 0.7f, 0.7f, 0.7, 1.0f };
-	GLfloat diffuse[] = { 0.6f, 0.6f, 0.6, 1.0f };
-	GLfloat specular[] = { 1.0f, 1.0f, 1.0, 1.0f };
-	GLfloat shininess[] = { 50 };
-	glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, ambient);
-	glMaterialfv(GL_FRONT, GL_DIFFUSE, diffuse);
-	glMaterialfv(GL_FRONT, GL_SPECULAR, specular);
-	glMaterialfv(GL_FRONT, GL_SHININESS, shininess);
-
-	GLfloat lightIntensity[] = { 0.7f, 0.7f, 1, 1.0f };
-	GLfloat lightPosition[] = { -7.0f, 6.0f, 3.0f, 0.0f };
-	glLightfv(GL_LIGHT0, GL_POSITION, lightIntensity);
-	glLightfv(GL_LIGHT0, GL_DIFFUSE, lightIntensity);
-}
-void setupCamera() {
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluPerspective(60, 640 / 480, 0.001, 100);
-
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-	camera.look();
-}
-
-void Display() {
-	setupCamera();
-	setupLights();
-
+void Display(void) {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	glPushMatrix();
-	glTranslated(0.4, 0.4, 0.6);
-	glRotated(45, 0, 0, 1);
-	glScaled(0.08, 0.08, 0.08);
-	drawJack();
-	glPopMatrix();
-	glPushMatrix();
-	glTranslated(0.6, 0.38, 0.5);
-	glRotated(30, 0, 1, 0);
-	glutSolidTeapot(0.08);
-	glPopMatrix();
-	glPushMatrix();
-	glTranslated(0.25, 0.42, 0.35);
-	glutSolidSphere(0.1, 15, 15);
-	glPopMatrix();
-	glPushMatrix();
-	glTranslated(0.4, 0.0, 0.4);
-	drawTable(0.6, 0.02, 0.02, 0.3);
-	glPopMatrix();
-	drawWall(0.02);
-	glPushMatrix();
-	glRotated(90, 0, 0, 1.0);
-	drawWall(0.02);
-	glPopMatrix();
-	glPushMatrix();
-	glRotated(-90, 1.0, 0.0, 0.0);
-	drawWall(0.02);
-	glPopMatrix();
+	glLoadIdentity();
 
+	glTranslatef(0, 0, -zoom);
+	glRotatef(rotx, 1, 0, 0);
+	glRotatef(roty, 0, 1, 0);
+	glRotatef(rotz, 0, 0, 1);
+	
+	drawHouse();
+	drawGrid();
 	glFlush();
 }
 
-void Keyboard(unsigned char key, int x, int y) {
-	float d = 0.01;
-
-	switch (key) {
-	case 'w':
-		camera.moveY(d);
-		break;
-	case 's':
-		camera.moveY(-d);
-		break;
-	case 'a':
-		camera.moveX(d);
-		break;
-	case 'd':
-		camera.moveX(-d);
-		break;
-	case 'q':
-		camera.moveZ(d);
-		break;
-	case 'e':
-		camera.moveZ(-d);
-		break;
-
-	case GLUT_KEY_ESCAPE:
-		exit(EXIT_SUCCESS);
+void Motion(int x, int y)
+{
+	if (view)
+		return;
+	int diffx = x - lastx;
+	int diffy = y - lasty;
+	lastx = x;
+	lasty = y;
+	if (Buttons[0])
+	{
+		rotx += (float) 0.5f * diffy;
+		roty += (float) 0.5f * diffx;
 	}
-
-	glutPostRedisplay();
-}
-void Special(int key, int x, int y) {
-	float a = 1.0;
-
-	switch (key) {
-	case GLUT_KEY_UP:
-		camera.rotateX(a);
-		break;
-	case GLUT_KEY_DOWN:
-		camera.rotateX(-a);
-		break;
-	case GLUT_KEY_LEFT:
-		camera.rotateY(a);
-		break;
-	case GLUT_KEY_RIGHT:
-		camera.rotateY(-a);
-		break;
-	}
-
 	glutPostRedisplay();
 }
 
-void main(int argc, char** argv) {
-	glutInit(&argc, argv);
+void Mouse(int b, int s, int x, int y)
+{
+	if (view)
+		return;
+	lastx = x;
+	lasty = y;
+	switch (b)
+	{
+	case GLUT_LEFT_BUTTON:
+		Buttons[0] = ((GLUT_DOWN == s) ? 1 : 0);
+		break;
+	case GLUT_MIDDLE_BUTTON:
+		Buttons[1] = ((GLUT_DOWN == s) ? 1 : 0);
+		break;
+	case GLUT_RIGHT_BUTTON:
+		Buttons[2] = ((GLUT_DOWN == s) ? 1 : 0);
+		break;
+	default:
+		break;
+	}
+	glutPostRedisplay();
+}
 
-	glutInitWindowSize(640, 480);
-	glutInitWindowPosition(50, 50);
+void Anim() {
+	glLoadIdentity();
+	gluLookAt(7.0f, 7.0f, 0.0f, 0.0f, 0.0f, 0.01f, 0.0f, 1.0f, 0.0f);
+	glutPostRedisplay();
+}
 
-	glutCreateWindow("Lab 5");
-	glutDisplayFunc(Display);
-	glutKeyboardFunc(Keyboard);
-	glutSpecialFunc(Special);
+void key(unsigned char key, int x, int y) {
+	if (key == 'p' || key == 'P') {
+		paused = !paused;
+	}
+	
+	if (key == 'v') {
+		view = !view;
+		if (view) {
+			rotx = 45;
+			roty = 180;
+		}
+		
+	}
+	if (view) {
+		return;
+	}
+	if (key=='=')
+	{
+		// Zoom in
+		zoom -= 0.3f;
+	}
+	
+	if(key=='-')
+	{
+		// Zoom out
+		zoom += 0.3f;
+	}
+}
 
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
-	glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
-
-	glEnable(GL_DEPTH_TEST);
+void initTiles() {
+	float currX =-2, currZ;
+	for (int i = 0; i < gridRows; i++) {
+		currZ = 4;
+		for (int j = 0; j < gridCols; j++) {
+			tiles[i][j] = Tile(currX, 0 , currZ, 0.0, 0.0, 0.0);
+			currZ -= 1;
+		}
+		tiles[i][7].occupied = true;
+		tiles[i][7].character = 'r';
+		currX += 1;
+	}
+	
+}
+void light() {
 	glEnable(GL_LIGHTING);
 	glEnable(GL_LIGHT0);
 	glEnable(GL_NORMALIZE);
 	glEnable(GL_COLOR_MATERIAL);
-
 	glShadeModel(GL_SMOOTH);
+}
+void main(int argc, char** argv) {
+	
+	initTiles();
+	glutInit(&argc, argv);
+
+	glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+	glutInitWindowPosition(350, 100);
+
+	glutCreateWindow("Plants vs Zombies");
+	glutDisplayFunc(Display);
+	glutIdleFunc(Anim);
+	glutKeyboardFunc(key);
+	glutMouseFunc(Mouse);
+	glutMotionFunc(Motion);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
+	glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
+
+	glEnable(GL_DEPTH_TEST);
+	light();
+	
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(45.0f, WINDOW_WIDTH / WINDOW_HEIGHT, 0.01f, 300.0f);
+
+	glMatrixMode(GL_MODELVIEW);
 
 	glutMainLoop();
 }
